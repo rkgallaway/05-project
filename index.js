aed18303d986ec079645bf7104dc54b71067861c
george <geomarray@gmail.com>
2018-12-26 13:53:15 -0800

    have four working methods

--- index.js
+++ index.js
@@ -54,8 +54,8 @@ const transforms = {
   greyscale: require('./transformers/greyscale'),
   invert: require('./transformers/invert'),
   test: require('./transformers/test'),
-  blueLines: require('./transformers/black-to-blue'),
-  infared: require('./transformers/infared'),
+  infrared: require('./transformers/infrared'),
+  shrek: require('./transformers/shrek'),
 };
 
 // ------------------ GET TO WORK ------------------- //
--- transformers/shrek.js
+++ transformers/shrek.js
@@ -7,5 +7,5 @@ module.exports = (bmp) => {
   for(let i = 0; i < bmp.colorArray.length; i+=4) {
     bmp.colorArray[i+1] = 255;
   }
-  console.log('turning the blacks to blues');
+  console.log('entering the swamp..');
 };
